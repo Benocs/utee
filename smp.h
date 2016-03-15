59de9253c90a0b25ab7c5faa17c98b4d24048f4d
Robert Wuttke <robert@benocs.com>
2016-03-15 15:45:15 +0100

    [smp] add some more atomic operations that C can't guarantee us
    Signed-off-by: Robert Wuttke <robert@benocs.com>

--- smp.h
+++ smp.h
@@ -12,6 +12,64 @@ typedef struct {
     uint64_t counter;
 } atomic_t;
 
+/*
+ * Atomic operations that C can't guarantee us.  Useful for
+ * resource counting etc..
+ */
+
+#define ATOMIC_INIT(i)  { (i) }
+
+/**
+ * atomic_read - read atomic variable
+ * @v: pointer of type atomic_t
+ *
+ * Atomically reads the value of @v.
+ */
+static inline int atomic_read(const atomic_t *v)
+{
+    return (*(volatile int *)&(v)->counter);
+}
+
+/**
+ * atomic_set - set atomic variable
+ * @v: pointer of type atomic_t
+ * @i: required value
+ *
+ * Atomically sets the value of @v to @i.
+ */
+static inline void atomic_set(atomic_t *v, int i)
+{
+    v->counter = i;
+}
+
+/**
+ * atomic_add - add integer to atomic variable
+ * @i: integer value to add
+ * @v: pointer of type atomic_t
+ *
+ * Atomically adds @i to @v.
+ */
+static inline void atomic_add(int i, atomic_t *v)
+{
+    asm volatile(LOCK_PREFIX "addl %1,%0"
+             : "+m" (v->counter)
+             : "ir" (i));
+}
+
+/**
+ * atomic_sub - subtract integer from atomic variable
+ * @i: integer value to subtract
+ * @v: pointer of type atomic_t
+ *
+ * Atomically subtracts @i from @v.
+ */
+static inline void atomic_sub(int i, atomic_t *v)
+{
+    asm volatile(LOCK_PREFIX "subl %1,%0"
+             : "+m" (v->counter)
+             : "ir" (i));
+}
+
 /**
  * atomic_inc - increment atomic variable
  * @v: pointer of type atomic_t
